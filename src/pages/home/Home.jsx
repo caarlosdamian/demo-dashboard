0c7d6041ce70b44e9fb38bf0dcbb7529cde9012a
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-03-21 10:59:59 -0600

    feat: add chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,5 @@
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
@@ -5,16 +7,23 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-     <Sidebar/>
-     <div className="homeContainer">
-       <Navbar/>
-      <div className="widgets">
-        <Widget type='user'/>
-        <Widget type='order'/>
-        <Widget type='earning'/>
-        <Widget type='balance'/>
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured/>
+          <Chart/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Lates Transactions</div>
+        </div>
       </div>
-     </div>
     </div>
   );
 };
