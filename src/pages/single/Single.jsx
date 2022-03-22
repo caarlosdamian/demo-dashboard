be343f6161e7f22abec94f9c4b394998fe3ae55e
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-03-21 23:27:18 -0600

    feat: add home page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './single.scss'
 
 const Single = () => {
+  let params = useParams();
+  console.log(params)
   return (
-    <div>Single</div>
+    <div className='single'>
+      <Sidebar/>
+      <div className="singleConatiner">
+        <Navbar/>
+        <div className="top">
+          <div className="left">
+            <div className="editButton">
+              Edit
+            </div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="itemImg" />
+            </div>
+            details
+          </div>
+          <div className="right">
+
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
   )
 }
 
